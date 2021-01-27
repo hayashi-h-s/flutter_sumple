29c9e6d1db5be70e394d5e7fc74abc5d5830417b
hayamizu_shinji <hayamizu.hnut@gmail.com>
2021-01-27 09:20:48 +0900

    リストビューを使用

--- lib/main.dart
+++ lib/main.dart
@@ -3,61 +3,61 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp()); /*1*/
 
-class MyApp extends StatelessWidget {/*2*/
-
-  // ここに変数ではだめ
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // // ここに変数をいれなければならない
-    // final wordPair = WordPair.random();
-    // return MaterialApp(
-    //   title: 'Welcome to Flutter',
-    //   home: Scaffold(
-    //     appBar: AppBar(
-    //       title: Text('Welcome to Flutter'),
-    //     ),
-    //     body: Center(
-    //       child: Text(wordPair.asPascalCase),
-    //     ),
-    //   ),
-    // );
-
-
-    // StatefulWidget を使用した場合
-    https://flutter.ctrnost.com/tutorial/tutorial05/
-      final wordPair = WordPair.random();
-      return MaterialApp(
-        title: 'Welcome to Flutter',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Welcome to Flutter'),
-          ),
-          body: Center(
-            child: RandomWords(),
-          ),
-        ),
-      );
+    return MaterialApp(
+        title: 'Startup Name Generator',
+        home: RandomWords()
+    );
   }
 }
 
+class RandomWordsState extends State<RandomWords> {
 
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
-// StatelessWidgetで扱うあたいはすべて不変的でありプロパティを変更することはできません
-// StatefulWidgetではウィジェットの生存期間中に変更される値を維持することができ、実装するには、少なくとも2つのクラスが必要
+  @override
+  Widget build(BuildContext context) {  return Scaffold(
+    appBar: AppBar(
+      title: Text('Startup Name Generator'),
+    ),
+    body: _buildSuggestions(),
+  );
+  }
 
-// Stateクラスのインスタンスを作成するStatefulWidgetクラス
-// Stateクラス (状態)
+  // リストビューを作成するメソッド
+  Widget _buildSuggestions() {
+    // リストビューを作成
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          // 線を返している
+          if (i.isOdd) return Divider();
 
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            // リストを追加する処理
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          // リストタイルを作成
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
 
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
 }
 
-class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
-}
